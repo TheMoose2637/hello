df624f784f5be30dfea2474d286d0e44457f7b03
TheMoose <mpatterson20@huskers.unl.edu>
2025-08-28 14:50:44 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author:Moose Patterson
+ *        mpatterson20@huskers.unl.edu
+ * Date: 8/28/2025
+ *
+ * A simple name change and added major in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello world, I'm Moose Patterson!\n");
+    printf("My major is electrical engineering!\n");
+
+    return 0;
+}))
